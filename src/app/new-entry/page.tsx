c4403abc59fbc3ba4c23d30a863bfea6ceb85e9f
takaza-codes <elekovalenko@gmail.com>
2025-08-31 17:31:07 +0300

    basic form and styling added

--- src/app/new-entry/page.tsx
+++ src/app/new-entry/page.tsx
@@ -6,6 +6,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { cardAdded } from "../store/slices/CardsSlice";
 import { Film } from "../../../types";
 import { RootState } from "../store/store";
+import CustomButton from "@/components/CustomButton";
 
 export default function AddFilmForm() {
   const dispatch = useDispatch();
@@ -32,21 +33,27 @@ export default function AddFilmForm() {
 
     dispatch(cardAdded(newFilm));
     alert("New film added");
+    console.log("New film added:", newFilm);
     reset();
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} noValidate>
-      <div>
+    <main className="mt-6 flex flex-col flex-1">
+      <h1 className="my-10 text-3xl text-stone-800 font-bold text-center ">
+        Create your own entry
+      </h1>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        noValidate
+        className="mx-auto grid grid-cols-[30% 1fr] gap-4 text-2xl max-w-2xl p-4">
         <label>Title:</label>
         <input
           {...register("title", { required: "Title is required" })}
           type="text"
+          className="form-input"
         />
-        {errors.title && <p style={{ color: "red" }}>{errors.title.message}</p>}
-      </div>
+        {errors.title && <p className="error-msg">{errors.title.message}</p>}
 
-      <div>
         <label>Episode ID:</label>
         <input
           {...register("episode_id", {
@@ -55,47 +62,49 @@ export default function AddFilmForm() {
             min: { value: 1, message: "Episode ID must be positive" },
           })}
           type="number"
+          className="form-input"
         />
         {errors.episode_id && (
-          <p style={{ color: "red" }}>{errors.episode_id.message}</p>
+          <p className="error-msg">{errors.episode_id.message}</p>
         )}
-      </div>
 
-      <div>
         <label>Opening:</label>
         <textarea
           {...register("opening_crawl", {
             required: "Opening crawl is required",
           })}
+          className="form-input"
         />
         {errors.opening_crawl && (
-          <p style={{ color: "red" }}>{errors.opening_crawl.message}</p>
+          <p className="error-msg">{errors.opening_crawl.message}</p>
         )}
-      </div>
 
-      <div>
         <label>Director:</label>
         <input
           {...register("director", { required: "Director is required" })}
           type="text"
+          className="form-input"
         />
         {errors.director && (
-          <p style={{ color: "red" }}>{errors.director.message}</p>
+          <p className="error-msg">{errors.director.message}</p>
         )}
-      </div>
 
-      <div>
         <label>Producer:</label>
         <input
           {...register("producer", { required: "Producer is required" })}
           type="text"
+          className="form-input"
         />
         {errors.producer && (
-          <p style={{ color: "red" }}>{errors.producer.message}</p>
+          <p className="error-msg">{errors.producer.message}</p>
         )}
-      </div>
 
-      <button type="submit">Add Film</button>
-    </form>
+        <CustomButton
+          type="submit"
+          className="custom-button col-start-1 col-end-3 justify-self-center mt-4">
+          Add film
+        </CustomButton>
+      </form>
+    </main>
   );
 }
